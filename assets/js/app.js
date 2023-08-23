2f695073e7b5b80893fb909770f7b72c281d457f
davidmercereau <david@mercereau.info>
2023-08-23 17:42:11 +0200

    Trad + Ubat Ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,30 +4,6 @@ function debug(msg) {
     }
 }
 
-// Envoi du formulaire
-function submitForm() {
-    debug('Soumission du formulaire');
-    //var error
-    // Vérification pour calcul
-    // Faire apparaître les résultats
-    $("#result").show();
-    document.getElementById("result").scrollIntoView();
-    $("#submit_input").val(1);
-    hashChange();
-    // Méthode G, niveau 1
-    if ($("#level").val() == 1) {
-        // Pour la méthode de calcul
-        debug('Level 1');
-        $(".res_temp_indor").replaceWith($("#temp_indor").val());
-        $(".res_temp_base").replaceWith($("#temp_base").val());
-        $(".res_volume").replaceWith($("#livingvolume").val());
-        $(".res_g").replaceWith($("#g").val());
-        var resDeperditionMax = precise_round(( $("#temp_indor").val() - $("#temp_base").val() ) * $("#livingvolume").val() * $("#g").val(), 2);
-        $(".res_level1").replaceWith(resDeperditionMax);
-    }
-    $("#resDeperditionMax").replaceWith(precise_round(resDeperditionMax/1000, 2));
-}
-
 // https://www.w3resource.com/javascript-exercises/javascript-math-exercise-14.php
 function precise_round(n, r) {
     let int = Math.floor(n).toString()
@@ -223,6 +199,11 @@ $( document ).ready(function() {
             debug("HTML5 : invalid form");
         }
     });
+    $("#calcShowHide").on( "click", function(e) {
+        $(".calcul_level"+$("#level").val()).toggle();
+        debug("toggle calcul")
+    });
+    
 
 
     ////////////////////////////
