c9dad86287e78b88411d96e90651e39bc94a51a9
davidmercereau <david@mercereau.info>
2023-09-22 11:38:56 +0200

    N'utiliser que les "cas d'usage critique" pour le dimensionnement

--- assets/js/app-suggestion.js
+++ assets/js/app-suggestion.js
@@ -18,7 +18,7 @@ function suggestion() {
             '<table id="suggestionTab">'
                 +'<thead><tr>'
                     +'<th data-i18n="[html]suggestion-name" rowspan="2">Name</th>'
-                    +'<th data-i18n="[html]suggestion-per-day"  colspan="3">Per day</th>'
+                    +'<th data-i18n="[html]suggestion-per-day"  colspan="4">Per day</th>'
                     +'<th data-i18n="[html]suggestion-weight" rowspan="2">Weight</th>'
                     +'<th data-i18n="[html]suggestion-link" rowspan="2">link</th>'
                     +'<th data-i18n="[html]suggestion-diffpowerdeperdition" rowspan="2">Difference with need</th>'
@@ -26,6 +26,7 @@ function suggestion() {
                     +'<th data-i18n="[html]suggestion-power">Power</th>'
                     +'<th data-i18n="[html]suggestion-fire">Fire</th>'
                     +'<th data-i18n="[html]suggestion-wood">Wood</th>'
+                    +'<th data-i18n="[html]suggestion-use">use</th>'
                 +'</tr></thead>'
                 +'<tbody>'
                 +'</tbody>'
@@ -40,35 +41,38 @@ function suggestion() {
         $.each(settings.pdmData, function() {
             var pdmData = this;
             $.each(this.dalyPower, function() {
-                id=id+1;
-                var diffPowerDeperdition = this.power-resDeperdition;
-                var diffPowerDeperditionAbs = Math.abs(diffPowerDeperdition);
-                var diffPowerDeperditionPercent =  100*diffPowerDeperdition/resDeperdition;
-                $("#suggestionTransparentConsole ul").append('<li>'+id+' : '+pdmData.name+' '+this.power+'W. La différence avec le besoin est de '+diffPowerDeperdition+'</li>');
-                if (diffPowerDeperditionAbs < bestDiffPowerDeperdition) {
-                    bestId=id;
-                    bestDiffPowerDeperdition=diffPowerDeperditionAbs;
-                    $("#suggestionTransparentConsole ul").append('<li><b>=> C\'est, pour le moment, le poêle qui s\'approche le plus du besoin</b></li>');
+                if (this.use == "critical") {
+                    id=id+1;
+                    var diffPowerDeperdition = this.power-resDeperdition;
+                    var diffPowerDeperditionAbs = Math.abs(diffPowerDeperdition);
+                    var diffPowerDeperditionPercent =  100*diffPowerDeperdition/resDeperdition;
+                    $("#suggestionTransparentConsole ul").append('<li>'+id+' : '+pdmData.name+' '+this.power+'W. La différence avec le besoin est de '+diffPowerDeperdition+'</li>');
+                    if (diffPowerDeperditionAbs < bestDiffPowerDeperdition) {
+                        bestId=id;
+                        bestDiffPowerDeperdition=diffPowerDeperditionAbs;
+                        $("#suggestionTransparentConsole ul").append('<li><b>=> C\'est, pour le moment, le poêle qui s\'approche le plus du besoin</b></li>');
+                    }
+                    //debug("diffPowerDeperdition="+diffPowerDeperdition);
+                    if (Math.abs(diffPowerDeperditionPercent) < settings.pdmSuggestion.percentPowerSuper) {
+                        trClass='bg-success-subtle';
+                    } else if (Math.abs(diffPowerDeperditionPercent) < settings.pdmSuggestion.percentPowerCool) {
+                        trClass='bg-warning-subtle';
+                    } else {
+                        trClass='text-secondary';
+                    }
+                    $('#suggestionTab > tbody:last-child').append(
+                        '<tr class="'+trClass+'">'
+                            +'<td>'+pdmData.name+'</td>'
+                            +'<td class="text-center">'+precise_round(this.power/1000,2)+'kW</td>'
+                            +'<td class="text-center">'+this.fire+'</td>'
+                            +'<td class="text-center">'+this.woodLoad+'kg</td>'
+                            +'<td class="text-center">'+this.use+'</td>'
+                            +'<td class="text-center">'+pdmData.weight+'kg</td>'
+                            +'<td class="text-center"><a href="'+pdmData.link+'">link</a></td>'
+                            +'<td class="text-center">'+precise_round(diffPowerDeperditionAbs/1000, 2)+' ('+Math.round(diffPowerDeperditionPercent)+'%)</td>'
+                        +'<tr>'
+                    );
                 }
-                //debug("diffPowerDeperdition="+diffPowerDeperdition);
-                if (Math.abs(diffPowerDeperditionPercent) < settings.pdmSuggestion.percentPowerSuper) {
-                    trClass='bg-success-subtle';
-                } else if (Math.abs(diffPowerDeperditionPercent) < settings.pdmSuggestion.percentPowerCool) {
-                    trClass='bg-warning-subtle';
-                } else {
-                    trClass='text-secondary';
-                }
-                $('#suggestionTab > tbody:last-child').append(
-                    '<tr class="'+trClass+'">'
-                        +'<td>'+pdmData.name+'</td>'
-                        +'<td class="text-center">'+precise_round(this.power/1000,2)+'kW</td>'
-                        +'<td class="text-center">'+this.fire+'</td>'
-                        +'<td class="text-center">'+this.woodLoad+'kg</td>'
-                        +'<td class="text-center">'+pdmData.weight+'kg</td>'
-                        +'<td class="text-center"><a href="'+pdmData.link+'">link</a></td>'
-                        +'<td class="text-center">'+precise_round(diffPowerDeperditionAbs/1000, 2)+' ('+Math.round(diffPowerDeperditionPercent)+'%)</td>'
-                    +'<tr>'
-                );
             });
         });
         debug("Best ID = "+bestId);
