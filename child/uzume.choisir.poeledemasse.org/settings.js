6ffbbf9a727aa8bd0bac3c5be610be83fd10ca8d
davidmercereau <david@mercereau.info>
2024-04-25 15:08:59 +0200

    Suppression du maxi & utilisation de 2 ou 3 flambée par jour

--- child/uzume.choisir.poeledemasse.org/settings.js
+++ child/uzume.choisir.poeledemasse.org/settings.js
@@ -14,7 +14,7 @@ const userSettings = {
       "name": "[Uzume] Mini Batchblock",
       "dalyPower": [ 
         { "fire": 1, "woodLoad": 12, "power": 1600, "use": "normal" },
-        { "fire": 2, "woodLoad": 12, "power": 3300, "use": "normal" },
+        { "fire": 2, "woodLoad": 12, "power": 3300, "use": "critical" },
         { "fire": 3, "woodLoad": 12, "power": 4900, "use": "critical" }
       ],
       "weight": 1200,
@@ -26,7 +26,7 @@ const userSettings = {
       "name": "[Uzume] Cuisinière Batchblock ",
       "dalyPower": [ 
         { "fire": 1, "woodLoad": 12, "power": 1600, "use": "normal" },
-        { "fire": 2, "woodLoad": 12, "power": 3300, "use": "normal" },
+        { "fire": 2, "woodLoad": 12, "power": 3300, "use": "critical" },
         { "fire": 3, "woodLoad": 12, "power": 4900, "use": "critical" }
       ],
       "weight": 1200,
@@ -38,23 +38,11 @@ const userSettings = {
       "name": "[Uzume] Medi Batchblock",
       "dalyPower": [ 
         { "fire": 1, "woodLoad": 18, "power": 2500, "use": "normal" },
-        { "fire": 2, "woodLoad": 18, "power": 5000, "use": "normal" },
+        { "fire": 2, "woodLoad": 18, "power": 5000, "use": "critical" },
         { "fire": 3, "woodLoad": 18, "power": 7500, "use": "critical" }
       ],
       "weight": 1550,
       "link": "https://www.uzume.fr/plans-poeles-de-masse/Medi-Batchblock",
-      "comment": "",
-    },
-    {
-      // https://www.uzume.fr/quelle-puissance-pour-mon-poele-de-masse
-      "name": "[Uzume] Maxi Batchblock avec banc",
-      "dalyPower": [ 
-        { "fire": 1, "woodLoad": 28, "power": 3900, "use": "normal" },
-        { "fire": 2, "woodLoad": 28, "power": 7700, "use": "normal" },
-        { "fire": 3, "woodLoad": 28, "power": 11600, "use": "critical" }
-      ],
-      "weight": 2000,
-      "link": "https://www.uzume.fr/plans-poeles-de-masse/Maxi-Batchblock",
       "comment": "",
     }
   ]
