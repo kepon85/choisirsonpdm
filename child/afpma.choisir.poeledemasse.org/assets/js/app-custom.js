8c46ec2f52d2de8c51b5f76313e415e38fc59a72
davidmercereau <david@mercereau.info>
2024-12-13 12:00:29 +0100

    Omission

--- child/afpma.choisir.poeledemasse.org/assets/js/app-custom.js
+++ child/afpma.choisir.poeledemasse.org/assets/js/app-custom.js
@@ -1,4 +1,3 @@
-$('#title').html('<h2 data-i18n="[html]afpma-app-title">Is MiniMasse right for you?</h2>');
 $("#submit_button").attr("data-i18n", "afpma-app-title");
 $('#nav-cartenf-tab').click();
 $('#help').hide();
