3a1d8c6f8465210ef3fcf5e0cc757b838df9d3be
davidmercereau <david@mercereau.info>
2025-03-06 11:08:06 +0100

    Amélioration interface expert

--- assets/js/app-function.js
+++ assets/js/app-function.js
@@ -107,11 +107,13 @@ function wallCheck(wallId) {
         $('.wall-check-' + wallId).addClass('bg-danger-subtle');
         $('.wall-check-' + wallId).removeClass('bg-success-subtle');
         $('#wall-' + wallId + '-check-svg').hide();
+        $('#wall-' + wallId + '-nocheck-svg').show();
         $('#wall-check-' + wallId).val(0);
     } else {
         $('.wall-check-' + wallId).removeClass('bg-danger-subtle');
         $('.wall-check-' + wallId).addClass('bg-success-subtle');
         $('#wall-' + wallId + '-check-svg').show();
+        $('#wall-' + wallId + '-nocheck-svg').hide();
         $('#wall-check-' + wallId).val(1);
     }
 }
@@ -615,9 +617,10 @@ function detailBuildingAddWall(id = null) {
     $('#addWallButton').before(
         + '<tbody class="wall-sortable">'
         + '<tr id="wall-' + wallId + '" class="wall-' + wallId + ' walls wall-check">'
-            + '<td class="wall-check-' + wallId + '">'
+            + '<td class="align-middle text-center wall-check-' + wallId + '">'
                 + '<input class="debug" type="hidden" id="wall-check-' + wallId + '" name="wall-check" value="0" />'
-                + '<svg style="display: none;" id="wall-' + wallId + '-check-svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path class="bg-primary-subtle border border-primary-subtle " d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>'
+                + '<svg style="display: none;" id="wall-' + wallId + '-check-svg" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path class="bg-primary-subtle border-primary-subtle " d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>'
+                + '<svg data-toggle="tooltip" title="Il manque des informations pour calculer cette paroi" style="display: none;" id="wall-' + wallId + '-nocheck-svg"  xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path class="bg-danger-subtle border-danger-subtle" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>'
             + '</td>'
             + '<td>'
                 + '<input class="debug" type="hidden" id="wall-' + wallId + '-window-id" name="wall-' + wallId + '-window-id" value="0" />'
@@ -627,7 +630,7 @@ function detailBuildingAddWall(id = null) {
                 + '<select name="wall-type[]" id="wall-type-' + wallId + '" style="width: 100%;" class="wall-type form-select form-control hashchange">'
                 + '</select>'
             + '</td>'
-            + '<td class="wall-rsi-rse-popup text-center">'
+            + '<td class="align-middle text-center wall-rsi-rse-popup text-center">'
                 +'<input type="hidden" class="form-control hashchange" name="wall-rsi[]" id="wall-rsi-' + wallId + '" />'
                 +'<span id="wall-rsi-' + wallId + '-val" class="wall-rse-val">'
                     + '<button data-toggle="tooltip" title="Clique ici pour définir un RSI" type="button" class="btn btn-primary btn-icon">'
@@ -637,7 +640,7 @@ function detailBuildingAddWall(id = null) {
                     + '</button>'
                 +'</span>'
             + '</td>'
-            + '<td class="wall-rsi-rse-popup text-center">'
+            + '<td class="align-middle text-center wall-rsi-rse-popup text-center">'
                 + '<input type="hidden" class="form-control hashchange" name="wall-rse[]" id="wall-rse-' + wallId + '" />'
                 + '<span id="wall-rse-' + wallId + '-val" class="wall-rse-val">'
                     + '<button data-toggle="tooltip" title="Clique ici pour définir un RSE" type="button" class="btn btn-primary btn-icon">'
@@ -647,7 +650,7 @@ function detailBuildingAddWall(id = null) {
                     + '</button>'
                 +'</span>'
             + '</td>'
-            + '<td class="wall-bridge-popup text-center">'
+            + '<td class="align-middle text-center wall-bridge-popup text-center">'
                 + '<input type="hidden" class="form-control hashchange " name="wall-bridge[]" id="wall-bridge-' + wallId + '" />'
                 + '<span id="wall-bridge-' + wallId + '-val" class="wall-bridge-val" >'
                     + '<button data-toggle="tooltip" title="Clique ici pour définir un pont thermique" type="button" class="btn btn-secondary btn-icon">'
